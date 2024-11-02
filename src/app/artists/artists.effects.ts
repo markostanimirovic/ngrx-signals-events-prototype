f2c0c4286d2c37d92137e70dc828c7cf85336ffc
markostanimirovic <markostanimirovic95@gmail.com>
2024-11-02 23:23:33 +0100

    remove redundant feature input

--- src/app/artists/artists.effects.ts
+++ src/app/artists/artists.effects.ts
@@ -1,17 +1,14 @@
 import { inject } from '@angular/core';
 import { exhaustMap } from 'rxjs';
-import { signalStoreFeature, type } from '@ngrx/signals';
-import { EntityState } from '@ngrx/signals/entities';
+import { signalStoreFeature } from '@ngrx/signals';
 import { Events, withEffects } from '@ngrx/signals/events';
 import { mapResponse } from '@ngrx/operators';
 import { withDisplayErrorEffect } from '@/shared/state/display-error-effect.feature';
-import { Artist } from './artist.model';
 import { ArtistsService } from './artists.service';
 import { artistsApiEvents, artistsPageEvents } from './artists.events';
 
 export function withArtistsEffects() {
   return signalStoreFeature(
-    { state: type<EntityState<Artist>>() },
     withEffects(
       (
         _,
